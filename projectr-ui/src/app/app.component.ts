71964d39ec7e22f2acdf8918bda714be94d51cb5
Sairam <sairampasupuleti.42@gmail.com>
2019-06-28 00:49:27 +0530

    ProjectR UI initial commit 0.0.1

---
+++ projectr-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-r-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'projectr-ui';
+}
